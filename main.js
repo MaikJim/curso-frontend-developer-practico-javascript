ad23c2b826e6a2143df61adcbede6c4dfb9c0fe1
MaikJim <maikoljimenez427@gmail.com>
2022-10-12 19:22:28 -0300

    feat: Agregando los componentes del menu y el carrito de compras

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
 }
